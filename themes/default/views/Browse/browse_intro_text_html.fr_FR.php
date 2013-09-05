961a008b47b1ca837672e844bbc4d35c2ab3b999
gautiermichelin <gm@ideesculture.com>
2013-09-05 21:12:35 +0200

    French l10n : translated views texts
    French l10n : translated views texts
    
    Signed-off-by: gautiermichelin <gm@ideesculture.com>

---
+++ themes/default/views/Browse/browse_intro_text_html.fr_FR.php
@@ -0,0 +1,8 @@
+<div class="textContent">
+	<div>
+		La page Explorer autorise vos utilisateurs à trouver des objets au sein de votre collection en fonction des "facettes" de recherche de votre choix, tel que le type d'objet, la date, les mots clés, les lieux ou les entités (personnes physiques, institutions...).  Le panneau Explorer est automatiquement généré en fonction des métadonnées de vos enregistrements, de cette façon vos utilisateurs n'obtiendront jamais un résultat de recherche sans résultat.
+	</div>
+	<div>
+		Vous pouvez configurer les facettes à afficher en modifiant le fichier <a href="http://wiki.collectiveaccess.org/index.php?title=Browse_Configuration" target="_blank">browse.conf</a> dans l'application Pawtucket. Vous pouvez modifier ce texte en modifiant le fichier views/Browse/browse_intro_text_html.fr_FR.php dans le répertoire de votre thème.
+	</div>
+</div>
