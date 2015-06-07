a50f6684d953ea15a53e5c2e8bc498e490470805
Stefan <stefan@whirl-i-gig.com>
2015-06-07 16:23:09 -0400

    NOJIRA Add WWSF

---
+++ app/plugins/wwsf/views/themes_intro_text_html.php
@@ -0,0 +1,15 @@
+<?php
+global $g_ui_locale;
+
+			if ($g_ui_locale == 'en_US') {
+?>
+				THEMES offers albums specially compiled by the website editors.
+				<br /><br />Under “Exhibition” you can find the six chronological chapters of private records that were central to the 2009 exhibition <a href="http://www.deutsche-kinemathek.de/en/exhibitions/past/2009/moments-in-time" target="_blank">"Moments in Time 1989/1990"</a>in the Museum for Film and Television in Berlin.
+<?php
+			}else{
+?>
+				THEMEN bietet von der Redaktion zusammengestellte Alben zu kennzeichnenden wie auch unvermuteten Ereignissen, Stationen, Personen und Motiven der Zeit. 
+				<br /><br />Unter „Ausstellung“ finden Sie die sechs chronologischen Kapitel mit Privataufnahmen, die 2009 den Schwerpunkt der Ausstellung <a href="http://www.deutsche-kinemathek.de/de/ausstellungen/rueckblick/2009/wir-waren-so-frei" target="_blank">"Wir waren so frei... Momentaufnahmen 1989/1990"</a> im Museum für Film und Fernsehen in Berlin bildeten.
+<?php			
+			}
+?>
