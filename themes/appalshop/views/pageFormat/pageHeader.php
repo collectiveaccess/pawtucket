06f474d28818c646b6805ae20ab90eb6262b3719
Maria <maria@whirl-i-gig.com>
2016-03-15 12:42:47 -0400

    appalshop theme cleanup

--- themes/appalshop/views/pageFormat/pageHeader.php
+++ themes/appalshop/views/pageFormat/pageHeader.php
@@ -48,114 +48,12 @@
 
 		<div id="topBar">
 		<a href='http://www.appalshop.org'>Back to Appalshop website ></a>
-<?php
-/*
-			$vb_client_services = (bool)$this->request->config->get('enable_client_services');
-			if (!$this->request->config->get('dont_allow_registration_and_login')) {
-				if($this->request->isLoggedIn()){
-					$o_client_services_config = caGetClientServicesConfiguration();
-					if ($vb_client_services && (bool)$o_client_services_config->get('enable_user_communication')) {
-						//
-						// Unread client communications
-						//
-						$t_comm = new ca_commerce_communications();
-						$va_unread_messages = $t_comm->getMessages(array('unreadOnly' => true, 'user_id' => $this->request->getUserID()));
-						
-						$va_message_set_ids = array();
-						foreach($va_unread_messages as $vn_transaction_id => $va_messages) {
-							$va_message_set_ids[] = $va_messages[0]['set_id'];
-						}
-						
-					}
-					
-					if(!$this->request->config->get('disable_my_collections')){
-						# --- get all sets for user
-						$t_set = new ca_sets();
-						$va_sets = caExtractValuesByUserLocale($t_set->getSets(array('table' => 'ca_objects', 'user_id' => $this->request->getUserID())));
-						if(is_array($va_sets) && (sizeof($va_sets) > 1)){
-							print "<div id='lightboxLink'>
-										<a href='#' onclick='$(\"#lightboxList\").toggle(0, function(){
-																								if($(\"#lightboxLink\").hasClass(\"lightboxLinkActive\")) {
-																									$(\"#lightboxLink\").removeClass(\"lightboxLinkActive\");
-																								} else {
-																									$(\"#lightboxLink\").addClass(\"lightboxLinkActive\");
-																								}
-																								});')>Lightbox</a>";
-							if(is_array($va_message_set_ids) && sizeof($va_message_set_ids)){
-								print " <img src='".$this->request->getThemeUrlPath()."/graphics/icons/envelope.gif' border='0'>";
-							}
-							print "<div id='lightboxList'><b>"._t("your lightboxes").":</b><br/>";
-							foreach($va_sets as $va_set){
-								print caNavLink($this->request, ((strlen($va_set["name"]) > 30) ? substr($va_set["name"], 0, 30)."..." : $va_set["name"]), "", "", "Sets", "Index", array("set_id" => $va_set["set_id"]));
-								if($vb_client_services && is_array($va_message_set_ids) && in_array($va_set["set_id"], $va_message_set_ids)){
-									print " <img src='".$this->request->getThemeUrlPath()."/graphics/icons/envelope.gif' border='0'>";
-								}
-								print "<br/>";
-							}
-							print "</div>";
-							print "</div>";
-						}else{
-							print caNavLink($this->request, _t("Lightbox"), "", "", "Sets", "Index");
-							if($vb_client_services && is_array($va_message_set_ids) && sizeof($va_message_set_ids)){
-								print " <img src='".$this->request->getThemeUrlPath()."/graphics/icons/envelope.gif' border='0'>";
-							}
-						}
-					}
-					
-					if ($vb_client_services && (bool)$o_client_services_config->get('enable_my_account')) {
-						$t_order = new ca_commerce_orders();
-						if ($vn_num_open_orders = sizeof($va_orders = $t_order->getOrders(array('user_id' => $this->request->getUserID(), 'order_status' => array('OPEN', 'SUBMITTED', 'IN_PROCESSING', 'REOPENED'))))) {
-							print "<span style='color: #cc0000; font-weight: bold;'>".caNavLink($this->request, _t("My Account (%1)", $vn_num_open_orders), "", "", "Account", "Index")."</span>";
-						} else {
-							print caNavLink($this->request, _t("My Account"), "", "", "Account", "Index");
-						}
-							
-					}				
-					
-					if($this->request->config->get('enable_bookmarks')){
-						print caNavLink($this->request, _t("My Bookmarks"), "", "", "Bookmarks", "Index");
-					}
-					print caNavLink($this->request, _t("Logout"), "", "", "LoginReg", "logout");
-				}else{
-					print caNavLink($this->request, _t("Login/Register"), "", "", "LoginReg", "form");
-				}
-			}
-			
-			# Locale selection
-			global $g_ui_locale;
-			$vs_base_url = $this->request->getRequestUrl();
-			$vs_base_url = ((substr($vs_base_url, 0, 1) == '/') ? $vs_base_url : '/'.$vs_base_url);
-			$vs_base_url = str_replace("/lang/[A-Za-z_]+", "", $vs_base_url);
-			
-			if (is_array($va_ui_locales = $this->request->config->getList('ui_locales')) && (sizeof($va_ui_locales) > 1)) {
-				print caFormTag($this->request, $this->request->getAction(), 'caLocaleSelectorForm', null, 'get', 'multipart/form-data', '_top', array('disableUnsavedChangesWarning' => true));
-			
-				$va_locale_options = array();
-				foreach($va_ui_locales as $vs_locale) {
-					$va_parts = explode('_', $vs_locale);
-					$vs_lang_name = Zend_Locale::getTranslation(strtolower($va_parts[0]), 'language', strtolower($va_parts[0]));
-					$va_locale_options[$vs_lang_name] = $vs_locale;
-				}
-				print caHTMLSelect('lang', $va_locale_options, array('id' => 'caLocaleSelectorSelect', 'onchange' => 'window.location = \''.caNavUrl($this->request, $this->request->getModulePath(), $this->request->getController(), $this->request->getAction(), array('lang' => '')).'\' + jQuery(\'#caLocaleSelectorSelect\').val();'), array('value' => $g_ui_locale, 'dontConvertAttributeQuotesToEntities' => true));
-				print "</form>\n";
-			
-			}
-*/			
-?>
 		</div><!-- end topbar -->
 		<div id="pageArea">
 			<div id="header">
 <?php
 				print caNavLink($this->request, "<img src='".$this->request->getThemeUrlPath()."/graphics/spacer.gif' width='300' height='80' border='0'>", "", "", "", "");
 		print "<div id='headerLinks'>";	
-// 				print "<div class='headerLink'><a href=''>About</a></div>";
-// 				print "<div class='headerLink'><a href='http://archive.appalshop.org/news'>News</a></div>";
-// 				print "<div class='headerLink'><a href='http://archive.appalshop.org/news/?page_id=13'>Services</a></div>";
-// 				print "<div class='headerLink'><a href='http://archive.appalshop.org/news/?page_id=15'>Support</a></div>";
-// 				print "<div class='headerLink'><a href='http://archive.appalshop.org/news/?page_id=17'>Contact</a></div>";
-// 				print "<div class='headerLink'><a href='http://www.facebook.com' style='padding-right:5px;'><img src='".$this->request->getThemeUrlPath()."/graphics/f_logo.jpg' border='0'></a></div>";
-// 				print "<div class='headerLink'><a href='http://www.twitter.com' style='padding-right:5px;'><img src='".$this->request->getThemeUrlPath()."/graphics/imgres.jpg' border='0'></a></div>";
-
 ?>
 			<ul class='sf-menu'>
 				<li><a href='#'>About</a>
@@ -186,7 +84,6 @@
 				print caNavLink($this->request, _t("Browse The Database"), '', '', 'Browse', 'clearCriteria')." ";
 				print caNavLink($this->request, _t("Collections"), '', 'FindingAids', 'List', 'Index')." ";
 				print caNavLink($this->request, _t("Special Projects"), '', 'simpleGallery', 'Show', 'Index');
-				#print join(" ", $this->getVar('nav')->getHTMLMenuBarAsLinkArray());
 ?>			
 				<div id="search"><form name="header_search" action="<?php print caNavUrl($this->request, '', 'Search', 'Index'); ?>" method="get">
 						<a href="#" style="position: absolute; z-index:1500; margin: 2px 0px 0px 140px;" name="searchButtonSubmit" onclick="document.forms.header_search.submit(); return false;"><img src='<?php print $this->request->getThemeUrlPath(); ?>/graphics/searchglass.gif' width='13' height='13' border='0'></a>
