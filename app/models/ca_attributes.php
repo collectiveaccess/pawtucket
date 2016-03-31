005e8d019af0ad6c28359b3ad583c1faf53cf9a0
Seth <seth@collectiveaccess.org>
2016-03-31 19:13:28 -0400

    Start of library update

--- app/models/ca_attributes.php
+++ app/models/ca_attributes.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2008-2014 Whirl-i-Gig
+ * Copyright 2008-2015 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -517,13 +517,16 @@ class ca_attributes extends BaseModel {
 			$vs_errors = '';
 		}
 
-		$ps_formatted_element = str_replace("^LABEL", "<span id='_attribute_value_".$pa_element_info['element_code']."'>{$vs_label}</span>", $ps_format);
+		$ps_formatted_element = str_replace("^LABEL", "<span class='_attribute_value_".$pa_element_info['element_code']."'>{$vs_label}</span>", $ps_format);
 		$ps_formatted_element = str_replace("^ELEMENT", $vs_element, $ps_formatted_element);
 		$ps_formatted_element = str_replace("^DESCRIPTION", "", $ps_formatted_element);
 		$ps_formatted_element = str_replace("^EXTRA", "", $ps_formatted_element);
 	
 		if ($vs_description) {
-			TooltipManager::add('#_attribute_value_'.$pa_element_info['element_code'], "<h3>".$vs_label."</h3>".$vs_description);
+			// don't use TooltipManager to make sure the tooltip is also displayed when this element is added dynamically (via "add" button)
+			//TooltipManager::add('#_attribute_value_'.$pa_element_info['element_code'], "<h3>".$vs_label."</h3>".$vs_description);
+
+			$ps_formatted_element .= "\n".caGetTooltipJS(array('._attribute_value_'.$pa_element_info['element_code'] => $vs_description));
 		}
 		return $ps_formatted_element;
 	}
@@ -723,10 +726,12 @@ class ca_attributes extends BaseModel {
 	 */
 	static public function getAttributeCount($po_db, $pn_table_num, $pn_row_id, $pn_element_id) {
 		$qr_attrs = $po_db->query("
-			SELECT count(*) c
-			FROM ca_attributes caa
+			SELECT count(distinct caa.attribute_id) c
+			FROM ca_attributes caa, ca_attribute_values cav
 			WHERE
-				(caa.table_num = ?) AND (caa.row_id = ?) AND (caa.element_id = ?)
+				(cav.attribute_id = caa.attribute_id) AND
+				(caa.table_num = ?) AND (caa.row_id = ?) AND (caa.element_id = ?) AND
+				(cav.item_id IS NOT NULL OR cav.value_longtext1 IS NOT NULL OR cav.value_decimal1 IS NOT NULL OR cav.value_integer1 IS NOT NULL OR cav.value_blob IS NOT NULL)
 		", (int)$pn_table_num, (int)$pn_row_id, (int)$pn_element_id);
 		if ($po_db->numErrors()) {
 			//$this->errors = $po_db->errors;
@@ -851,5 +856,23 @@ class ca_attributes extends BaseModel {
 		return $va_values;
 	}
 	# ------------------------------------------------------
+	/**
+	 * Get code for element
+	 * @return string
+	 * @throws MemoryCacheInvalidParameterException
+	 */
+	public function getElementCode() {
+		if(!$this->getPrimaryKey()) { return false; }
+
+		if(MemoryCache::contains($this->getPrimaryKey(), 'AttributeToElementCodeCache')) {
+			return MemoryCache::fetch($this->getPrimaryKey(), 'AttributeToElementCodeCache');
+		}
+
+		$t_element = new ca_metadata_elements($this->get('element_id'));
+		$vs_element_code = $t_element->get('element_code');
+
+		MemoryCache::save($this->getPrimaryKey(), $vs_element_code, 'AttributeToElementCodeCache');
+		return $vs_element_code;
+	}
+	# ------------------------------------------------------
 }
-?>
