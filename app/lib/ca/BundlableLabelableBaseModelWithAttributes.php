9b9aa971ab808a338dec190d46d6282abfb6e721
Seth <seth@collectiveaccess.org>
2018-12-23 20:58:06 -0500

    NOJIRA MySQL 5.7 compatibility

--- app/lib/ca/BundlableLabelableBaseModelWithAttributes.php
+++ app/lib/ca/BundlableLabelableBaseModelWithAttributes.php
@@ -4850,9 +4850,11 @@ if (!$vb_batch) {
 			$vs_order_by = '';
 			if ($t_item_rel && $t_item_rel->hasField('rank')) {
 				$vs_order_by = ' ORDER BY '.$t_item_rel->tableName().'.rank';
+				$va_selects[] = $t_item_rel->tableName().'.rank';
 			} else {
 				if ($t_rel_item && ($vs_sort = $t_rel_item->getProperty('ID_NUMBERING_SORT_FIELD'))) {
 					$vs_order_by = " ORDER BY ".$t_rel_item->tableName().".{$vs_sort}";
+					$va_selects[] = $t_rel_item->tableName().".{$vs_sort}";
 				}
 			}
 			
